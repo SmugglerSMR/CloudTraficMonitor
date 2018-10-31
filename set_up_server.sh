cef97178494e7e4d5c1a6ed3c849912b91b0f2b2
Xana_Smuggler <mattsadov@gmail.com>
2018-10-31 12:42:04 +1000

    sudo fix

--- set_up_server.sh
+++ set_up_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update -y && apt install -y \
+sudo apt update -y && sudo apt install -y \
     make \
     python \
     gcc \
@@ -9,7 +9,7 @@ sudo apt update -y && apt install -y \
 
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt install nodejs
+sudo apt install -y nodejs
 
 npm install
 
